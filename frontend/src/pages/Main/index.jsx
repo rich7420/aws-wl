e8694b580e5c1068536b4e6c59d95b7827410d80
rich7420 <rc910420@gmail.com>
2025-04-26 12:02:05 +0800

    from charlie

--- frontend/src/pages/Main/index.jsx
+++ frontend/src/pages/Main/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import PasswordModal, { usePasswordModal } from "@/components/Modals/Password";
 import { FullScreenLoader } from "@/components/Preloader";
 import Home from "./Home";
-import NewWorkspaceModal, { useNewWorkspaceModal } from "@/components/Modals/NewWorkspace";
 import DefaultChatContainer from "@/components/DefaultChat";
 import { isMobile } from "react-device-detect";
 import Sidebar, { SidebarMobileHeader } from "@/components/Sidebar";
@@ -16,20 +15,10 @@ export default function Main() {
     return <>{requiresAuth !== null && <PasswordModal mode={mode} />}</>;
 
   const user = userFromStorage();
-  const { showing, showModal, hideModal } = useNewWorkspaceModal();
-
-  React.useEffect(() => {
-    if (user?.role !== "admin") {
-      showModal(); // 一進來就開啟 modal
-    }
-  }, []);
-
   return (
     <div className="w-screen h-screen overflow-hidden bg-theme-bg-container flex">
       {!isMobile ? <Sidebar /> : <SidebarMobileHeader />}
-      <div className="flex-1" />
-      {showing && <NewWorkspaceModal hideModal={hideModal} />}
+      {!!user && user?.role !== "admin" ? <DefaultChatContainer /> : <Home />}
     </div>
   );
-
 }

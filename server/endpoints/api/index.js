b2e983318508406e05979e434b1648cf1aeaa130
{LinSimon901101} <{acs109141@gm.ntcu.edu.tw}>
2025-04-26 22:16:32 +0800

    Add AWS transcribe api table

--- server/endpoints/api/index.js
+++ server/endpoints/api/index.js
@@ -8,6 +8,8 @@ const { apiWorkspaceThreadEndpoints } = require("./workspaceThread");
 const { apiUserManagementEndpoints } = require("./userManagement");
 const { apiOpenAICompatibleEndpoints } = require("./openai");
 const { apiEmbedEndpoints } = require("./embed");
+const { apiSpeechEndpoints } = require("./speech");
+
 
 // All endpoints must be documented and pass through the validApiKey Middleware.
 // How to JSDoc an endpoint
@@ -24,6 +26,7 @@ function developerEndpoints(app, router) {
   apiUserManagementEndpoints(router);
   apiOpenAICompatibleEndpoints(router);
   apiEmbedEndpoints(router);
+  apiSpeechEndpoints(router);
 }
 
 module.exports = { developerEndpoints };
